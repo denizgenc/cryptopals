bd6f81da7d7c420569cf765f3dadb830eb6aaba2
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2018-03-31 20:46:52 +0100

    Forgot to update one of the files with the previous change

--- set1/c1_hexto64.py
+++ set1/c1_hexto64.py
@@ -19,9 +19,3 @@ def hexto64(hexstring):
         b64list.append(base64str[num])
     
     return "".join(b64list)
-    
-hexstring = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-output = hexto64(hexstring)
-check = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-print(output)
-print("Am I right? ", output == check)
