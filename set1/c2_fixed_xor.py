4ba23bd386dfcc996b7c107c228ea3295a585faf
Deniz Genc <denizgenc1995@gmail.com>
2018-05-18 00:06:48 +0100

    Solution to Challenge 3 and other improvements
    Along with the solution, I've made an important fix to c2_fixed_xor.py that actually ensures that it outputs a properly spaced hex string (before, values from 0-15 were being encoded as single characters, which messes up the length of the string - especially important if you're trying to use the hex string to represent bytes). Also, I added commented note at the bottom of c1_hexto64.py for the "Baby version" (read: better solution using Python's bytes/bytearray classes/methods). Finally, I did not commit this directly as I worked on this from my brother's laptop so I didn't have access to git desktop and can't see those all important diffs, etc. I want to make sure they're all good later on before merging with master.

--- set1/c2_fixed_xor.py
+++ set1/c2_fixed_xor.py
@@ -12,6 +12,7 @@ def fixed_xor(hexstring1, hexstring2):
         byte1 = int(hexstring1[index:index+2],16)
         byte2 = int(hexstring2[index:index+2],16)
         xorbyte = byte1 ^ byte2 # https://wiki.python.org/moin/BitwiseOperators
-        bytelist.append(format(xorbyte, "x"))
-    
+        bytelist.append(format(xorbyte, "x").zfill(2))
+        # zfill zero pads a string so that we get 01010101 instead of 1111
+            
     return "".join(bytelist)
