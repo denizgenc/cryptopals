f22c2dc7aad87e44215bd619b6c8b218b2c6699a
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2018-09-28 15:38:57 +0100

    PEP8ifying some code, mostly removing whitespace

--- set1/c3_xor_decrypt.py
+++ set1/c3_xor_decrypt.py
@@ -11,21 +11,21 @@ def frequency_score(deciphered):
     Better = closer to 0.
     """
     frequency_dict = {'e': 12.02, 't': 9.10, 'a': 8.12, 'o': 7.68, 'i': 7.31,
-    'n': 6.95, 's': 6.28, 'r': 6.02, 'h': 5.92, 'd': 4.32, 'l': 3.98, 'u': 2.88,
-    'c': 2.71, 'm': 2.61, 'f': 2.3, 'y': 2.11, 'w': 2.09, 'g': 2.03, 'p': 1.82,
-    'b': 1.49, 'v': 1.11, 'k': 0.69, 'x': 0.17, 'q': 0.11, 'j': 0.1, 'z': 0.07}
-    
+                      'n': 6.95, 's': 6.28, 'r': 6.02, 'h': 5.92, 'd': 4.32, 'l': 3.98, 'u': 2.88,
+                      'c': 2.71, 'm': 2.61, 'f': 2.3, 'y': 2.11, 'w': 2.09, 'g': 2.03, 'p': 1.82,
+                      'b': 1.49, 'v': 1.11, 'k': 0.69, 'x': 0.17, 'q': 0.11, 'j': 0.1, 'z': 0.07}
+
     score = 0
     for char in frequency_dict.keys():
         proportion_in_deciphered = float(deciphered.count(char)) / len(deciphered)
         score += abs(proportion_in_deciphered - frequency_dict[char])
-        
+
     return score
 
 def xor_decrypt(encrypted):
     best_score = 100 # some high number
     best_guess = ""
-    for i in range(0,256):
+    for i in range(256):
         charstring = format(i, "x").zfill(2) * (len(encrypted)/2)
         # the above makes a string of repeating chars the same size as the string
         # we're decoding so that we can feed them both into the imported fixed_xor
@@ -37,5 +37,5 @@ def xor_decrypt(encrypted):
         if current_score < best_score:
             best_score = current_score
             best_guess = xored
-    
+
     return best_guess
