f1fea7f7fa51823b31c66dee18a927a165257719
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2018-09-28 18:10:00 +0100

    Getting closer, but hit upon a predictable problem: since I use scores as keys in a dictionary, I think I'm overwriting keys...

--- set1/c4_solution.py
+++ set1/c4_solution.py
@@ -3,6 +3,17 @@
 # (Your code from #3 should help.) 
 
 
-from c3_xor_decrypt import xor_decrypt, frequency_score
+from c3_xor_decrypt import xor_decrypt
 
+with open("4.txt") as file:
+    scores_and_strings = {}
+    for line in file:
+        decrypt, score = xor_decrypt(line.strip('\n')) # strip trailing newline
+        scores_and_strings[score] = [decrypt.decode(errors="ignore"), line]
+
+best_score = sorted(scores_and_strings)[0] # sort by keys, then get lowest key
+best = scores_and_strings[best_score] # index by that key to get values
+print(f"the best line had a score of {best_score}")
+print(f"The xored lines were {best[1]}")
+print(f"And, when decrypted, it was {best[0]}")
 
