5226d725bef562237485db95b60923349eae61b6
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2018-10-03 14:32:33 +0100

    Changed the Hamming distance function to use bytes objects as arguments, instead of strings

--- set1/c6_distance.py
+++ set1/c6_distance.py
@@ -1,19 +1,15 @@
-def hamming_distance(string_1, string_2, encoding='utf-8'):
-    """Returns the Hamming distance between 2 strings of a shared encoding
-    (UTF-8 by default).
-    """
-    bytes_1 = bytes(string_1, encoding)
-    bytes_2 = bytes(string_2, encoding)
+def hamming_distance(bytes_1, bytes_2):
+    """Returns the Hamming distance between 2 bytes objects."""
     distance = 0
 
     for char_1, char_2 in zip(bytes_1, bytes_2):
         xor = char_1 ^ char_2
         distance += f"{xor:b}".count('1')
 
-    # If strings are a different length, all the extra characters have
+    # If bytes objects are a different length, all the extra characters have
     # "different" bits, so add them to the distance
-    length_difference = abs(len(string_1) - len(string_2))
-    distance += (length_difference * 8) # 8 bits per character! Hopefully!
+    length_difference = abs(len(bytes_1) - len(bytes_1))
+    distance += (length_difference * 8) # 8 bits per byte
 
     return distance
 
