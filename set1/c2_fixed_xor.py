3f83fd091607dd5d30b3b65124b06772201247c3
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-04-20 22:53:08 +0100

    Remove commented out code in challenge 2

--- set1/c2_fixed_xor.py
+++ set1/c2_fixed_xor.py
@@ -14,15 +14,6 @@ def fixed_xor(hexstring1, hexstring2):
     xored_bytes = [byte1 ^ byte2 for byte1, byte2 in zip(first, second)]
     
     return bytes(xored_bytes)
-#    bytelist = []
-#    for index in range(0, len(hexstring1), 2):
-#        byte1 = int(hexstring1[index:index+2], 16)
-#        byte2 = int(hexstring2[index:index+2], 16)
-#        xorbyte = byte1 ^ byte2 # https://wiki.python.org/moin/BitwiseOperators
-#        bytelist.append(format(xorbyte, "x").zfill(2))
-#        # zfill zero pads a string so that we get 01010101 instead of 1111
-
-#    return "".join(bytelist)
 
 if __name__ == "__main__":
     string1 = "1c0111001f010100061a024b53535009181c"
